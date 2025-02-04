0b3af1b926c9ecc10624a6d51fe04e93df1a64b0
Prajualit Tickoo <23bec072@nith.ac.in>
2025-02-05 01:41:09 +0530

    route in app

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,17 +1,26 @@
-import express, { urlencoded } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+// Express app
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,   
-    credentials: true
-}));
+// CORS
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({ limit : "16kb" }));
+// Middlewares
+app.use(express.json({ limit: "16kb" }));
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/temp", express.static("temp"));
+
+// Routes
+import router from "./routes/user.route.js";
+app.use("/api/v1/user", router);
 
 export { app };
