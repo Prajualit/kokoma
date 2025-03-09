be522b1363462a2f2b494a799ff958538c9c8fdd
Prajualit Tickoo <23bec072@nith.ac.in>
2025-03-10 03:09:42 +0530

    Our products

---
+++ src/frontend/components/OurProducts/OurProducts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Products from './Products/Products'
+import WhyKokoma from './WhyKokoma/WhyKokoma'
+
+const OurProducts = () => {
+  return (
+    <>
+      <Products />
+      <WhyKokoma />
+    </>
+  )
+}
+
+export default OurProducts
---
+++ src/frontend/components/OurProducts/WhyKokoma/WhyKokoma.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhyKokoma = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WhyKokoma
