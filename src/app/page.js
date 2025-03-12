b2f58b09d74665bf7ce87f118f964ce76975e06e
Prajualit Tickoo <23bec072@nith.ac.in>
2025-03-13 00:12:01 +0530

    contact us

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Products from "@/frontend/components/OurProducts/Products/Products.jsx";
 import { useSelector } from "react-redux";
 import CartFloater from "@/frontend/components/Cart/CartFloater";
 import AboutUs from "@/frontend/components/AboutUs/AboutUs.jsx";
+import ContactUs from "@/frontend/components/ContactUs/ContactUs.jsx";
 
 export default function Home() {
   const activeNav = useSelector((state) => state.navbar.activeNav);
@@ -17,6 +18,7 @@ export default function Home() {
       {activeNav === "Home" && <Pagehome />}
       {activeNav === "Our Products" && <Products />}
       {activeNav === "About us" && <AboutUs />}
+      {activeNav === "Contact us" && <ContactUs />}
       <Footer />  
     </>
   );
---
+++ src/frontend/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactUs = () => {
+    return (
+        <div className='m-20 flex flex-col space-y-14 '>
+            <h1 className='text-[#112D4E] font-[600] text-[50px] leading-[100%] text-left  '>
+                Contact Us
+            </h1>
+        </div>
+    )
+}
+
+export default ContactUs
